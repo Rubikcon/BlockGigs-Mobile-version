41efa73abe68d54d7b7353289e3213d36b5e9ded
Moses <moseschizaram8@gmail.com>
2024-11-19 14:25:07 +0100

    Add color theme to sidebar

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { CiHome } from "react-icons/ci";
-import logo from "../../assets/logo1.svg";
+import logo from "../../assets/logo_.svg";
 import { FaCog } from "react-icons/fa";
 import { IoCubeSharp } from "react-icons/io5";
 import { RxDashboard } from "react-icons/rx";
@@ -17,13 +17,13 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
     const isActive = (path) => location.pathname === path;
 
     return (
-        <div className={`bg-[#1C2335] text-white h-screen ${isOpen ? "w-64" : "w-20"} transition-all duration-300 fixed top-0 left-0 flex flex-col`}>
+        <div className={`bg-[#2f66f6] text-white h-screen ${isOpen ? "w-64" : "w-20"} transition-all duration-300 fixed top-0 left-0 flex flex-col`}>
             {/* Logo Section */}
             <div className="flex items-center justify-center p-4 border-b border-gray-700">
                 <img
                     src={logo}
                     alt="Logo"
-                    className={`w-full ${isOpen ? "h-12" : "h-8"} transition-all`}
+                    className={`w-full ${isOpen ? "h-13" : "h-8"} transition-all`}
                 />
             </div>
 
@@ -43,7 +43,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/"
-                            className={`flex items-center p-2 ${isActive("/home") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/home") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <CiHome className="mr-3" />
                             {isOpen && <span>Home</span>}
@@ -52,7 +52,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard"
-                            className={`flex items-center p-2 ${isActive("/dashboard") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <RxDashboard className="mr-3" />
                             {isOpen && <span>Dashboard</span>}
@@ -61,7 +61,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/my-gigs"
-                            className={`flex items-center p-2 ${isActive("/dashboard/my-gigs") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/my-gigs") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <IoCubeSharp className="mr-3" />
                             {isOpen && <span>My Gigs</span>}
@@ -70,7 +70,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/discover"
-                            className={`flex items-center p-2 ${isActive("/dashboard/discover") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/discover") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <PiChartDonutFill className="mr-3" />
                             {isOpen && <span>Discover</span>}
@@ -79,7 +79,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/wallet"
-                            className={`flex items-center p-2 ${isActive("/dashboard/wallet") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/wallet") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <IoWallet className="mr-3" />
                             {isOpen && <span>Wallet</span>}
@@ -88,7 +88,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/messages"
-                            className={`flex items-center p-2 ${isActive("/dashboard/messages") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/messages") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <TbMessageFilled className="mr-3" />
                             {isOpen && <span>Chat</span>}
@@ -97,7 +97,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/offers"
-                            className={`flex items-center p-2 ${isActive("/dashboard/offers") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/offers") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <FaFolderClosed className="mr-3" />
                             {isOpen && <span>Offers & Application</span>}
@@ -106,7 +106,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <li>
                         <Link
                             to="/dashboard/settings"
-                            className={`flex items-center p-2 ${isActive("/dashboard/settings") ? "bg-white text-black rounded-lg" : "text-gray-300"} hover:text-white`}
+                            className={`flex items-center p-2 ${isActive("/dashboard/settings") ? "bg-white text-black rounded-lg" : "text-white-1200"} hover:text-black`}
                         >
                             <FaCog className="mr-3" />
                             {isOpen && <span>Settings</span>}
