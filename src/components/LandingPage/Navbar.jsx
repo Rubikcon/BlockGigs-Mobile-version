60a746feb8ab49c81baa8e9120fff0bf1d27f219
Moses <moseschizaram8@gmail.com>
2024-11-19 12:32:48 +0100

    Fix client-side navigation on navbar

--- src/components/LandingPage/Navbar.jsx
+++ src/components/LandingPage/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { CiMenuFries } from "react-icons/ci";
 import { LiaTimesSolid } from "react-icons/lia";
 import { connectWallet } from "../../utils/WalletUtils"
+import { Link } from 'react-router-dom';
 import "./style.css"
 
 const Navbar = () => {
@@ -38,7 +39,10 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex justify-between items-center gap-9">
         <li><a className='text-[#2f66f6] font-extrabold text-base' href="/">Home</a></li>
-        <li><a className='text-[#696f8c] font-medium text-base' href="/dashboard">Dashboard</a></li>
+        {/* <li><a className='text-[#696f8c] font-medium text-base' href="/dashboard">Dashboard</a></li> */}
+        <Link to="/dashboard" className='no -underline text-[#2f66f6] font-extrabold text-base'>
+          Dashboard
+        </Link>
         <li><a className='text-[#696f8c] font-medium text-base' href="#talents">Browse Talents</a></li>
         {/* <button
 
@@ -60,7 +64,11 @@ const Navbar = () => {
       {open && (
         <ul className="fixed top-14 left-0 w-full h-screen flex flex-col items-center justify-center gap-8 bg-gradient-to-r from-[#ecf3ff] via-[#ffffff] to-[#ecf3ff] z-40">
           <li><a href="/" className='text-[#2f66f6] font-extrabold text-lg' onClick={toggleNav}>Home</a></li>
-          <li><a href="/dashboard" className='text-[#696f8c] font-medium text-lg' onClick={toggleNav}>Dashboard</a></li>
+          {/* <li><a href="/dashboard" className='text-[#696f8c] font-medium text-lg' onClick={toggleNav}>Dashboard</a></li> */}
+          <Link to="/dashboard" className='no -underline text-[#696f8c] font-medium text-lg' onClick={toggleNav}>
+            Dashboard
+          </Link>
+
           <li><a href="#talents" className='text-[#696f8c] font-medium text-lg' onClick={toggleNav}>Browse Talents</a></li>
           {/* <button onClick={handleConnectWallet} className='px-4 py-2 bg-[#2F66F6] text-white font-medium rounded'>Connect Wallet</button> */}
           {!account ? (<button className='px-4 py-2 bg-[#2F66F6] text-white font-medium rounded' onClick={handleConnectWallet}>Connect Wallet</button>) : (<div className='wallet-info'><span className='wallet-address'>Connected: {account.slice(0, 6) + '...' + account.slice(-4)}</span>
